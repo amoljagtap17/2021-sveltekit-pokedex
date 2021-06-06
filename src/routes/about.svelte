6dc3cda5f618c7639c6813c7af92918f6a325555
Amol Jagtap <amoljagtap17@gmail.com>
2021-06-06 13:28:49 +0530

    SVELTEKIT_POKEDEX: add routes

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>Svelte Kit Pokedex | About</title>
+</svelte:head>
+
+<h1>About Page</h1>
+<p>This is really cool Svelte Kit demo that uses the Pokemon API to build a Pokedex.</p>
