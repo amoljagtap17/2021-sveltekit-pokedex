b36a167c53d9676697129ebe91da37eeede5c0aa
Amol Jagtap <amoljagtap17@gmail.com>
2021-06-06 21:35:31 +0530

    SVELTEKIT_POKEDEX: add active class to nav links

--- src/components/Nav/index.svelte
+++ src/components/Nav/index.svelte
@@ -1,4 +1,11 @@
+<script>
+	export let path
+</script>
+
 <nav class="flex justify-end w-full bg-gray-500 shadow-2xl">
-	<a href="/" class="mx-4 py-2 text-lg border-b-4 border-yellow-400">Home</a>
-	<a href="/about" class="mx-4 py-2 text-lg">About</a>
+	<a href="/" class={`mx-4 py-2 text-lg ${path === '/' && 'border-b-4 border-yellow-400'}`}>Home</a>
+	<a
+		href="/about"
+		class={`mx-4 py-2 text-lg ${path === '/about' && 'border-b-4 border-yellow-400'}`}>About</a
+	>
 </nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,20 @@
-<script>
-	import Nav from '../components/Nav/index.svelte'
+<script context="module">
+	export async function load({ page }) {
+		return {
+			props: {
+				path: page.path
+			}
+		}
+	}
 </script>
 
-<Nav />
+<script>
+	import Nav from '../components/Nav/index.svelte'
+
+	export let path
+</script>
+
+<Nav {path} />
 <div class="container mx-auto my-4">
 	<slot />
 </div>
