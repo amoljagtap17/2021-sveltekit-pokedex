dd57f4bbb67e3ac48314856be8f9ad91beb51edc
Amol Jagtap <amoljagtap17@gmail.com>
2021-06-06 14:10:25 +0530

    SVELTEKIT_POKEDEX: display data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,13 @@
+<script>
+	import { pokemon } from '../stores/pokestore'
+</script>
+
 <svelte:head>
 	<title>Svelte Kit Pokedex | Home</title>
 </svelte:head>
 
 <h1>Svelte Kit Pokedex</h1>
+
+{#each $pokemon as pokeman}
+	<p>{pokeman.name}</p>
+{/each}
