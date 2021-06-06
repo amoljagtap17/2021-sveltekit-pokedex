eeef8ab1f15f978284a449b9467f7b5ea7845e8d
Amol Jagtap <amoljagtap17@gmail.com>
2021-06-06 20:31:03 +0530

    SVELTEKIT_POKEDEX: add filtering

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,19 @@
 <script>
 	import { pokemon } from '../stores/pokestore'
 	import PokemanCard from '../components/PokemanCard/index.svelte'
+
+	let searchTerm = ''
+	let filteredPokemon = []
+
+	$: {
+		if (searchTerm) {
+			filteredPokemon = $pokemon.filter(pokeman =>
+				pokeman.name.toLowerCase().includes(searchTerm.toLowerCase())
+			)
+		} else {
+			filteredPokemon = [...$pokemon]
+		}
+	}
 </script>
 
 <svelte:head>
@@ -9,8 +22,15 @@
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
+<input
+	type="text"
+	placeholder="Search Pokemon"
+	class="w-full rounded-md text-lg p-4 border-2 focus:outline-none border-gray-200 focus:border-gray-400"
+	bind:value={searchTerm}
+/>
+
 <div class="py-4 grid gap-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
-	{#each $pokemon as pokeman}
+	{#each filteredPokemon as pokeman}
 		<PokemanCard {pokeman} />
 	{/each}
 </div>
